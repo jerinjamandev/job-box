2d80775b62042b40139bff0e8b8ff12536e613e0
jerinjamandev <jerinjaman.dev@gmail.com>
2025-05-08 22:02:43 +0600

    navbar-cng

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-base-100 lg:px-10 md:px-7 px-5">
+            <div className="navbar bg-base-100 lg:px-10 md:px-6 px-5">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
